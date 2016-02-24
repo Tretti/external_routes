0a850c3d5032971d7f71f5be358b419327c8de87
Ludvig Dickman <ludvig.dickman@tretti.se>
2016-02-24 13:03:56 +0100

    Only concat config/routes when present

--- lib/external_routes/rails/engine.rb
+++ lib/external_routes/rails/engine.rb
@@ -7,7 +7,7 @@ module ExternalRoutes
         instance_eval do
           def routes
             routes = super
-            routes.draw_paths.concat paths["config/routes"].paths
+            routes.draw_paths.concat paths["config/routes"].paths if paths["config/routes"].present?
             routes
           end
         end
